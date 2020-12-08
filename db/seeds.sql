301a05aabfda3e5e6a9512b840a3f1090a8482e4
joolyenIV <julianivey@gmail.com>
2020-12-07 21:09:07 -0600

    adding db files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("hamburger", false), ("cheeseburger", false), ("bacon double cheeseburger", false), ("turkey burger", false), ("mushroom and swiss burger", false);
