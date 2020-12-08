9d4685f36a7db553a53c93c2f847512a7d41ed18
joolyenIV <julianivey@gmail.com>
2020-12-08 00:15:01 -0600

    moving project up

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-var express = require("express");
+var express = require("express");   
+var exphbs = require("express-handlebars");    
+var bodyParser = require("body-parser");                            
+
+var routes = require("./controllers/burgers_controller.js");      
+
+var app = express();                                           
+var PORT = process.env.PORT || 2377;                              
+
+app.use(express.static("public"));                                
+app.use(bodyParser.json());                                         
+app.use(bodyParser.urlencoded({ extended: true }));                 
+
+//handlebars references
+app.engine("handlebars", exphbs({                                 
+    defaultLayout: "main"                                        
+}));
+
+app.set("view engine", "handlebars");                               
+
+app.use(routes);                                                 
+
+app.listen(PORT, function(){                             
+console.log("Listening on Port: " + PORT);
+});
